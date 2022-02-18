b7ac63f7a8a231075f33805100032340f7361acb
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-18 16:33:12 -0600

    Update 18_twoSum.py

--- 18_twoSum.py
+++ 18_twoSum.py
@@ -1,10 +1,9 @@
+# create a function that returns the numbers from an array that add up to target
 def twoSum(arr, target):
-  result = []
-  
-  for i in range(len(arr)-1):
-    for j in range(i+1, len(arr)):
-      if arr[i] + arr[j] == target:
-        result.append([arr[i], arr[j]])
-  print(result)
+    for i in range(len(arr)-1):
+        for j in range(i + 1, len(arr)):
+            if arr[i] + arr[j] == target:
+                return [arr[i], arr[j]]
 
-twoSum([1,2,3,4,5], 8)
+
+print(twoSum([1,2,3,4,5], 8))
