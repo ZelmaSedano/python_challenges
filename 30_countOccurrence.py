3437eb7b2e95746f41d7faba946e92ce48e2faad
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-19 17:21:05 -0600

    Create 30_countOccurrence.py

---
+++ 30_countOccurrence.py
@@ -0,0 +1,6 @@
+# create a function that returns how many times something is present inside string
+def occurrence(str, target):
+  return str.count(target)
+
+print(occurrence('bye bye bye', 'bye'))
+
