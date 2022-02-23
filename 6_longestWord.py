ded15109bbf98c42114813b2e0c925b5b029b0fa
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-23 11:13:43 -0600

    Update 6_longestWord.py

--- 6_longestWord.py
+++ 6_longestWord.py
@@ -1,5 +1,13 @@
-def longestWord(str):
-  longest = max(str.split(), key=len)
-  print(longest)
+def wordCount(str):
+    if len(str) == 0:
+        return 'please add some letters'
+    
+    for i in str:
+        if len(str) == 1:
+            if i == ' ':
+                return 'please add letter'
 
-longestWord('hi there')
+    arr = str.split(' ')
+    return len(arr)
+
+print(wordCount('hi there'))
