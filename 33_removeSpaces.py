700206444c562188606a8270e02a37ceb3a36a7d
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-24 16:08:47 -0600

    Update 33_removeSpaces.py

--- 33_removeSpaces.py
+++ 33_removeSpaces.py
@@ -1,9 +1,8 @@
-def removeSpaces(str):
+def remove_spaces(str):
     # edge case: if str is empty, return message
-    if len(str) == 0:
+    if len(str) == 0 or str.isspace():
         return 'please add some words to your string'
-    elif str.isspace():
-        return 'please add some words to your string'
+
     return str.replace(' ', '')
 
-print(removeSpaces(' '))
+print(remove_spaces('hi mom'))
