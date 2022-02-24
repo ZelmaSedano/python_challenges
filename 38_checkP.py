ca4d97e34d3b1aecd36ec17abcc233cf5e623bb6
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-24 15:56:52 -0600

    Update 38_checkP.py

--- 38_checkP.py
+++ 38_checkP.py
@@ -1,10 +1,19 @@
 def checkP(str):
+    # edge case: make sure string isn't empty or is just empty spaces
+    if len(str) == 0 or str.isspace():
+        return 'please add some words to your string'
+        #throw error instead?
+    
+    # create a count variable
     count = 0
+
+    # loop through str, adding to count if current element is '(', and subtracting is it's ')'
     for i in str:
         if i == '(':
-            count+=1
+            count += 1
         elif i == ')':
-            count-=1
+            count -= 1
+
     return count == 0
 
 print(checkP('()('))
