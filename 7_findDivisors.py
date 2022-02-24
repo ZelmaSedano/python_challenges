897c078ffdc1b251947062b593b2dbfc57eb4675
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-24 16:21:15 -0600

    Update 7_findDivisors.py

--- 7_findDivisors.py
+++ 7_findDivisors.py
@@ -1,10 +1,14 @@
-def findDivisors(num):
-  result = []
+def find_divisors(num = None):
+    if num is None:
+        return 'please add an argument'
 
-  for i in range(2, num):
-    if(num % i == 0):
-      result.append(i)
-  
-  print(result)
+    # since we are returning multiple divisors, we should add them to a list:
+    result = []
 
-findDivisors(72)
+    for i in range(2, num):
+        if(num % i == 0):
+            result.append(i)
+
+    return result
+
+print(find_divisors(72))
