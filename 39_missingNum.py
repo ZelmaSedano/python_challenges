789c957eadb066b36a2f4c21dae450ba7469e4b2
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-24 16:02:17 -0600

    Update 39_missingNum.py

--- 39_missingNum.py
+++ 39_missingNum.py
@@ -1,9 +1,13 @@
-def missing_num(arr):
-    list = []
-    for i in range(arr[0], arr[-1]+1):
-        if i not in arr:
-            list.append(i)
+def missing_num(list):
+    # check to make sure arr isn't empty
+    if not list:
+        return 'please add items to your list'
     
-    return list
-    
-print(missing_num([1,2,3,5]))
+    for i in range(list[0], list[-1]):
+        if i not in list:
+            return i
+            
+
+    return 'no missing numbers'
+
+print(missing_num([1,2,3,4,6]))
