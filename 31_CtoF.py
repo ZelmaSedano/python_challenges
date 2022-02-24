c82c5bc1147231a2a2a09e47e8b090d01453059a
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-24 16:19:24 -0600

    Update 31_CtoF.py

--- 31_CtoF.py
+++ 31_CtoF.py
@@ -1,5 +1,10 @@
-def convertFtoC(num):
-    cel = (num -32) * 5/9
-    return cel
+# you have to add = None to work
+def convert_f_to_c(num = None):
+    # edge case: if num is empty, return a message, cuz 0 is a valid temp
+    if num is None:
+        return 'please add a temp'
 
-print(convertFtoC(32))
+    celsius = (num - 32) * 5/9
+    return celsius
+
+print(convert_f_to_c())
