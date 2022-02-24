1966cea872d2234ae9afe48aeb3dedffa5cf1370
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-24 15:49:43 -0600

    Update 5_wordCount.py

--- 5_wordCount.py
+++ 5_wordCount.py
@@ -1,4 +1,11 @@
 def wordCount(str):
-  return len(str.split(' '))
+    if len(str) == 0:
+        return 'please add some words to your string'
+
+    # split the string into a list
+    list = str.split(' ')
+
+    # return the length of the list
+    return len(list)
 
 print(wordCount('hi there'))
