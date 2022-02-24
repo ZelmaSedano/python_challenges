214f6b8f21ce29d665f4a08633e610d3bb36f2ff
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-24 16:06:59 -0600

    Update 39_missingNum.py

--- 39_missingNum.py
+++ 39_missingNum.py
@@ -3,6 +3,7 @@ def missing_num(list):
     if not list:
         return 'please add items to your list'
     
+    # make sure to start from 0 to last element, otherwise it won't work
     for i in range(list[0], list[-1]):
         if i not in list:
             return i
