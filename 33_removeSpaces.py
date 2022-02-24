75bfeb9beeb693cff3951cb8bb63f9241001408f
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-24 15:52:25 -0600

    Update 33_removeSpaces.py

--- 33_removeSpaces.py
+++ 33_removeSpaces.py
@@ -1,4 +1,9 @@
 def removeSpaces(str):
+    # edge case: if str is empty, return message
+    if len(str) == 0:
+        return 'please add some words to your string'
+    elif str.isspace():
+        return 'please add some words to your string'
     return str.replace(' ', '')
 
-print(removeSpaces('hi there'))
+print(removeSpaces(' '))
