a819715bb6faf7c3487989a0d9aa012e842e58b1
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-28 10:02:20 -0600

    Update 38_checkP.py

--- 38_checkP.py
+++ 38_checkP.py
@@ -1,13 +1,13 @@
+from tabnanny import check
+
+
 def checkP(str):
-    # edge case: make sure string isn't empty or is just empty spaces
-    if len(str) == 0 or str.isspace():
-        return 'please add some words to your string'
-        #throw error instead?
-    
-    # create a count variable
+    if not str or str.isspace():
+        return 'please add some letters to your string'
+    # create a counter variable
     count = 0
 
-    # loop through str, adding to count if current element is '(', and subtracting is it's ')'
+    # loop through string, looking for ( & )
     for i in str:
         if i == '(':
             count += 1
@@ -16,4 +16,4 @@ def checkP(str):
 
     return count == 0
 
-print(checkP('()('))
+print(checkP(' '))
