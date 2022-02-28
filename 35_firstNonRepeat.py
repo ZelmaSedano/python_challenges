4a9c99fece5e48979059120903b3f2a8f05bcd68
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-02-28 10:30:51 -0600

    Update 35_firstNonRepeat.py

--- 35_firstNonRepeat.py
+++ 35_firstNonRepeat.py
@@ -1,15 +1,30 @@
-def firstNonRepeatingChar(str1):
-    char_order = []
-    counts = {}
-    for c in str1:
-        if c in counts:
-            counts[c] += 1
-        else:
-            counts[c] = 1
-            char_order.append(c)
-    for c in char_order:
-        if counts[c] == 1:
-            return c
-    return None
+def firstNonRepeat(str):
+    if not str or str.isspace():
+        return 'please add letters to your string'
+    
+    # create an empty list to keep the characters in order, b/c python doesn't keep them in their original order
+    order = []
+    # create an empty dictionary to hold the characters
+    chars = {}
 
-print(firstNonRepeatingChar('hi'))
+    # loop through the str
+    for c in str:
+        # if c is already in chars dict, add to its value by 1
+        if c not in chars:
+            chars[c] = 1
+            order.append(c)
+        else:
+            chars[c] += 1
+    # print(order)
+    # print(chars)
+
+    # loop through order list
+    for c in order:
+        # if the value of c in dictionary is 1
+        if chars[c] == 1:
+            return c
+
+    #backup 
+    return 'no non-repeating characters'
+
+print(firstNonRepeat('hi'))
