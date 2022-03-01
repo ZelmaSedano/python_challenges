6f8227b2c72c2c2b2c790490b76f7a45167398a6
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-01 10:50:14 -0600

    Update 11_sortLetters.py

--- 11_sortLetters.py
+++ 11_sortLetters.py
@@ -1,4 +1,10 @@
-def sortLetters(str):
+def sort_letters(str):
+    if not str:
+        return 'please add letters to string'
+
+    # remove the spaces
+    str = str.replace(' ', '')
+    # sort the letters, then join outside & return
     return ''.join(sorted(str))
 
-print(sortLetters('what'))
+print(sort_letters(''))
