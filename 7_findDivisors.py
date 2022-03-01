ebbe53e6249f67d230bbe53d1e8a1644b434b0da
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-01 10:35:45 -0600

    Update 7_findDivisors.py

--- 7_findDivisors.py
+++ 7_findDivisors.py
@@ -1,14 +1,18 @@
-def find_divisors(num = None):
-    if num is None:
-        return 'please add an argument'
+def find_divisors(num=None):
+    if num == None:
+        return 'please enter a number'
 
-    # since we are returning multiple divisors, we should add them to a list:
+    # create an empty list to hold divisors
     result = []
 
+    # loop from 2 to num, not including num
     for i in range(2, num):
-        if(num % i == 0):
+        if num % i == 0:
             result.append(i)
 
-    return result
+    if len(result) == 0:
+        return 'no divisors, number is prime'
+    else:
+        return result
 
-print(find_divisors(72))
+print(find_divisors())
