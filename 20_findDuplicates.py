7643fdc8504a1758ab5a216fe9159287092d2aa1
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-02 10:22:49 -0600

    Update 20_findDuplicates.py

--- 20_findDuplicates.py
+++ 20_findDuplicates.py
@@ -1,9 +1,16 @@
-def findDuplicates(arr):
+def find_dupes(list):
+    # create a result list to return
     result = []
-    for i in range(len(arr) - 1):
-        for j in range(i+1, len(arr)):
-            if arr[i] == arr[j]:
-                result.append(arr[i])
-    print(result)
 
-findDuplicates([1,2,3,1,2])
+    # loop through the list
+    for i in range(len(list)-1):
+        for j in range(i+1, len(list)):
+            if list[i] == list[j]:
+                result.append(list[i])
+
+    if len(result) == 0:
+        return 'no duplicates'
+    else:
+        return set(result)
+
+print(find_dupes([1,2,3,3,4,4,4,4]))
