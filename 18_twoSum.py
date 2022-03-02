fc91ca5353936b3e353b63d5cf4b7a5885732a28
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-02 09:54:37 -0600

    Update 18_twoSum.py

--- 18_twoSum.py
+++ 18_twoSum.py
@@ -1,13 +1,18 @@
 def two_sum(list, target=None):
+    if not list and target is None:
+        return 'please add arguments'
     if not list:
-        return 'please add elements to your list'
+        return 'please add elements to list'
     if target is None:
-        return 'please add number to 2nd argument'
+        return 'please add number to 2nd arguemnt'
+
+    # loop through list once, then twice, skipping i
     for i in range(len(list)-1):
         for j in range(i+1, len(list)):
             if list[i] + list[j] == target:
                 return [list[i], list[j]]
 
-    return 'no numbers add up to target'
+    # backup
+    return 'no numbers from list add up to target'
 
-print(two_sum([], 5))
+print(two_sum([], ))
