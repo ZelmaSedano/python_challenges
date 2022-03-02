6ad9e91fe325c9683186ec94643453ba7130f8b1
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-02 15:52:51 -0600

    Update 37_splitListChunks.py

--- 37_splitListChunks.py
+++ 37_splitListChunks.py
@@ -1,8 +1,14 @@
-def chunks (list, n):
-    new_list = []
-    for i in range(0, len(list), n):
-        new_list.append(list[i:i+n])
+def chunks(list, num=None):
+    if not list:
+        return 'please add elements to list'
+    if num is None:
+        return 'please add a number to 2nd arg'
 
-    return new_list
+    result = []
+    # loop through list
+    for i in range(0, len(list), num):
+        result.append(list[i:i+num])
 
-print(chunks([1,2,3,4,5,6], 2))
+    return result
+
+print(chunks([0,1,2,3,4,5], 2))
