dc34769b6177b84aa9adfda401da6e21d9d8bc30
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-02 10:39:25 -0600

    Update 56_merge_and_sort.py

--- 56_merge_and_sort.py
+++ 56_merge_and_sort.py
@@ -1,5 +1,11 @@
 def merge_and_sort(list1, list2):
-    newList = list1+list2
-    return sorted(newList)
+    if not list1 and not list2:
+        return 'please add arguments'
+    if not list1:
+        return 'please add elements to list1'
+    if not list2:
+        return 'please add elements to list2'
+
+    return sorted(list1+list2)
 
 print(merge_and_sort([1,2,3], [3,2,1]))
