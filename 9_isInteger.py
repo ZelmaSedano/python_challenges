681fc3ba2b0cc3ceed29aedeab393fafe076e020
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-09 12:40:14 -0600

    Update 9_isInteger.py

--- 9_isInteger.py
+++ 9_isInteger.py
@@ -1,4 +1,7 @@
-def isInteger(num):
+def isInt(num=None):
+    if num is None:
+        return 'please add a number to the 2nd argument'
+
     return type(num) == int
 
-print(isInteger(15))
+print(isInt('hi'))
