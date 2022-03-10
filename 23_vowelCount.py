c6bbc672ab5b2475df2d9b631e784514b5b8a518
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-10 16:27:42 -0600

    Update 23_vowelCount.py

--- 23_vowelCount.py
+++ 23_vowelCount.py
@@ -1,19 +1,27 @@
-def vowel_count(str):
+def vowel_count(str=None):
+    if str is None:
+        return 'please add a string'
+    if not str or str.isspace():
+        return 'please add letters to string'
+    
+
     # create a count variable
     count = 0
+    # define what a vowel is
+    vowels = 'aeiou'
+    # lowercase all the letters
+    str.lower()
 
-    # create a set of vowels
-    vowels = set('aeiouAEIOU')
-
-    # loop through the str
+    # loop through str
     for c in str:
         if c in vowels:
             count += 1
 
-    # backup
+    # if count is 0, return no vowels
     if count == 0:
         return 'no vowels'
+    # otherwise, return count
     else:
         return count
 
-print(vowel_count('hi'))
+print(vowel_count())
