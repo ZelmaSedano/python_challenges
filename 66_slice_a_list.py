5c22e589bb2eaf5e3c2ccee97a7e7c23b1042e00
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-12 09:48:58 -0600

    Update 66_slice_a_list.py

--- 66_slice_a_list.py
+++ 66_slice_a_list.py
@@ -1,4 +1,9 @@
-def slice_list(list, start, stop):
+def slice_list(list=None, start=None, stop=None):
+    if list is None and start is None and stop is None:
+        return 'please add arguments'
+    if not list:
+        return 'please add elements to list'
+
     return list[start:stop]
 
 print(slice_list([0,1,2,3,4,5], 0, 3))
