ab7da22f2b5506e90141fd73205543b4a7b6d8bf
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-12 10:28:17 -0600

    Update 67_STRING_to_String.py

--- 67_STRING_to_String.py
+++ 67_STRING_to_String.py
@@ -1,16 +1,16 @@
-# create a function that takes a capitalized string with underscores and returns a regular string w/ the first letter of each word capitalized
-def STRING_to_String(str):
-    # edgecase
-    if not str:
+# given a string like: THIS_IS_A_STRING, transform in into: This Is A String
+def alter_string(str=None):
+    # edgecases: if arg empty or str empty
+    if str is None:
+        return 'please add string to argument'
+    elif not str or str.isspace():
         return 'please add letters to string'
 
-    # remove all the underscores
+    # change the underscores into spaces
     str = str.replace('_', ' ')
-
     # lowercase all the letters
     str = str.lower()
-
-    # title each word
+    # capitalize the first letter of each word
     return str.title()
 
-print(STRING_to_String('HI_THERE'))
+print(alter_string('THIS_IS_A_STRING'))
