ade984e6c954428e8bd309d182f4bc88ae9fcb35
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-14 10:35:37 -0500

    Update 12_factorial.py

--- 12_factorial.py
+++ 12_factorial.py
@@ -1,23 +1,19 @@
-def factorial(num=None):
-    if num is None:
-        return 'please add number to argument'
+def factorial(num):
+    # edgecases: negative numbers don't have a factorial
+    if num < 0:
+        return 'negative numbers do not have a factorial'
 
-    # create a result variable & set it to 1 since we'll be multiplying using it
+    # if num is 0, factorial is 1
+    if num == 0:
+        return 1
+
+    # create a result variable to hold result & set it to 1 b/c we're multiplying by it
     result = 1
 
-    # edgecase: if num is under 0, return message
-    if num < 0:
-        return 'Sorry, factorial does not exist this number'
-    # if num is 0, return message
-    elif num == 0:
-        return 'The factorial of 0 is 1'
-    else:
-        # loop from 1 to num, including num
-        for i in range(1, num+1):
-            result *= i
-    
-    # return the factorial
+    # loop from 1 to num, including num
+    for i in range(1, num+1):
+        result *= i
+
     return result
 
-print('the factorial is:')
 print(factorial(5))
