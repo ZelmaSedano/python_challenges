b240ddc5a0db250201389e47b00012116ffa8bd8
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-14 11:11:24 -0500

    Update 19_isNumPrime.py

--- 19_isNumPrime.py
+++ 19_isNumPrime.py
@@ -1,7 +1,15 @@
 def is_prime(num):
-    for n in range(2, int(num*1/2)+1):
-        if num % n == 0:
+    # edgecases: if num is below 0 or 1, return message that negative numbers and 1 can't be prime
+    if num < 0:
+        return 'only positive numbers can be prime'
+    if num == 1:
+        return '1 is not a prime number'
+
+    # loop from 2 to num, not including num
+    for i in range(2, num):
+        if num % i == 0:
             return False
+
     return True
 
-print(is_prime(27))
+print(is_prime(17))
