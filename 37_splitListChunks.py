8a6f1a9098ae77282668c2c18d7d99de6c1c6c10
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-14 10:48:39 -0500

    Update 37_splitListChunks.py

--- 37_splitListChunks.py
+++ 37_splitListChunks.py
@@ -1,16 +1,16 @@
-def split_list_chunks(list, num=None):
-    if not list and num is None:
-        return 'please add arguments'
+def split_list_chunks(list, num):
+    # edgecases: if list is empty or num is negative
     if not list:
         return 'please add elements to list'
-    if num is None:
-        return 'please add a number to 2nd argument'
+    if num < 0:
+        return 'please only use positive numbers'
 
-    # create a result variable to return
+    # create a result list to return
     result = []
 
-    # loop through list using 0, len(list), num
+    # loop through the list using: 0, len(list), num
     for i in range(0, len(list), num):
+        # add sections of the list to result using start:stop syntax
         result.append(list[i:i+num])
 
     return result
