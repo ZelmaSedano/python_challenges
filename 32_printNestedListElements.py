90d4f00fdbaaff8acc1999f51d0a0fdb7ff9eb34
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-14 10:44:20 -0500

    Update 32_printNestedListElements.py

--- 32_printNestedListElements.py
+++ 32_printNestedListElements.py
@@ -1,17 +1,17 @@
-def print_nested_list(arr=None):
-    if arr is None:
-        return 'please add a list'
-    if not arr:
+def print_nested_list(list1):
+    # edgecase: if list is empty
+    if not list:
         return 'please add elements to list'
 
-    # loop through list
-    for i in arr:
-        # if the current element is a list
+    # loop through the list
+    for i in list1:
+        # if current element is a list, loop through it using j
         if isinstance(i, list):
-            # loop through that list
             for j in i:
+                # print out j
                 print(j)
+        # if current element ISN'T a list, simply print i
         else:
             print(i)
 
-print_nested_list([1,2,3,[4, 5, [6]]])
+print_nested_list([1,2,3,[4,5,6,[7]]])
