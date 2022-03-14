28e003947b8ec7de9904fbf6e379a3c1f9d0739a
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-14 12:54:39 -0500

    Update 14_sumElements.py

--- 14_sumElements.py
+++ 14_sumElements.py
@@ -1,15 +1,8 @@
 def sum_list(list):
-    # edgecase: if list is empty
+    # edgecases: if list is empty
     if not list:
         return 'please add elements to list'
 
-    # create a result variable
-    result = 0
+    return sum(list)
 
-    # loop through list, adding to result with each loop
-    for i in list:
-        result += i
-
-    return result
-
-print(sum_list([1,2,3,4]))
+print(sum_list([1,2,3,4,5]))
