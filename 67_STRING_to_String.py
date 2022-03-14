7edda79dce0d113d48293a2e31baaa2e0621f23a
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-14 11:01:09 -0500

    Update 67_STRING_to_String.py

--- 67_STRING_to_String.py
+++ 67_STRING_to_String.py
@@ -1,16 +1,16 @@
-# given a string like: THIS_IS_A_STRING, transform in into: This Is A String
-def alter_string(str=None):
-    # edgecases: if arg empty or str empty
-    if str is None:
-        return 'please add string to argument'
-    elif not str or str.isspace():
+# given a string likst THIS_IS_A_STRING return a string like so: This Is A String
+def upstart_alter_str(str):
+    # edgecase: if string is empty
+    if not str or str.isspace():
         return 'please add letters to string'
 
-    # change the underscores into spaces
+    # change all the underscores to spaces
     str = str.replace('_', ' ')
+
     # lowercase all the letters
     str = str.lower()
-    # capitalize the first letter of each word
+
+    # capitalize the first letters of each word
     return str.title()
 
-print(alter_string('THIS_IS_A_STRING'))
+print(upstart_alter_str('THIS_IS_A_STRING'))
