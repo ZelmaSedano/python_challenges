b18cb59fe98a51df651b09721f1ff2431c474f43
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-15 12:42:00 -0500

    Update 57_isPermutation.py

--- 57_isPermutation.py
+++ 57_isPermutation.py
@@ -1,4 +1,11 @@
-def isPermutation(str1, str2):
+def is_permutation(str1, str2):
+    # edgecase: if string is empty
+    if not str1 or str1.isspace():
+        return 'please add letters to first string'
+    elif not str2 or str2.isspace():
+        return 'please add letters to second string'
+
+    # sort the two strings and compare
     return sorted(str1) == sorted(str2)
 
-print(isPermutation('howdy', 'dwhoy'))
+print(is_permutation('hello', 'llohe'))
