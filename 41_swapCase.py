4c1120f541e897d08b70ade922e800e95d624cc4
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-15 12:43:29 -0500

    Update 41_swapCase.py

--- 41_swapCase.py
+++ 41_swapCase.py
@@ -1,4 +1,8 @@
-def swapCase(str):
+def swap_case(str):
+    # edgecase: if string is empty
+    if not str or str.isspace():
+        return 'please add letters to string'
+
     return str.swapcase()
 
-print(swapCase('hI'))
+print(swap_case('Hi'))
