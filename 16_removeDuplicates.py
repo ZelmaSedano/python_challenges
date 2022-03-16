1f4d5bd5af422e457d8e017df12aa2f080656ddd
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-16 12:46:14 -0500

    Update 16_removeDuplicates.py

--- 16_removeDuplicates.py
+++ 16_removeDuplicates.py
@@ -1,4 +1,9 @@
-def removeDuplicates(list):
-    return set(list)
+def remove_duplicates(list1):
+    # edgecase: if list is empty
+    if not list1:
+        return 'please add elements to list'
 
-print(removeDuplicates([1,2,3,1,1,1]))
+    # remove the duplicates with a set
+    return list(set(list1))
+
+print(remove_duplicates([1,2,3,4,4,4,4]))
