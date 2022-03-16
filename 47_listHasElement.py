a80dfcb2469a7473b2ff63a768fdc3dceb376784
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-16 12:48:13 -0500

    Update 47_listHasElement.py

--- 47_listHasElement.py
+++ 47_listHasElement.py
@@ -1,4 +1,11 @@
-def listHasElement(list, item):
+def list_has_element(list, item):
+    # edgecase: if list is empty or item is negative number
+    if not list:
+        return 'please add elements to list'
+    elif item < 0:
+        return 'please only use positive numbers'
+
+    # check to see if item is list and return result
     return item in list
 
-print(listHasElement([1,2,3], 4))
+print(list_has_element([1,2,3,4], 5))
