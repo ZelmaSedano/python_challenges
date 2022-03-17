90cb23d586ef7bc0b627bef3c35173e65745f75a
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-17 12:46:10 -0500

    Update 51_commonDivisors.py

--- 51_commonDivisors.py
+++ 51_commonDivisors.py
@@ -1,18 +1,18 @@
 def common_divisors(num1, num2):
-    # edgecase: if one number is negative
-    if num1 > 0 and num2 < 0:
-        return 'please use only positive or negative numbers'
-    elif num1 < 0 and num2 > 0:
-        return 'please use only positive or negative numbers'
-    
-    # create a variable to return
+    # edgecase: if one number is positive and one is negative, they aren't divisors
+    if num1 < 0 and num2 > 0:
+        return 'please only use positive numbers'
+    elif num1 > 0 and num2 < 0:
+        return 'please only use positive numbers'
+
+    # create a result variable to return
     result = []
 
-    # loop through list
-    for i in range(2, num2+1):
-        if(num1 % i == 0 and num2 % i == 0):
+    # loop from 2 to num2, including num2
+    for i in range(1, num2+1):
+        if num1 % i == 0 and num2 % i == 0:
             result.append(i)
 
     return result
 
-print(common_divisors(15,30))
+print(common_divisors(6, 18))
