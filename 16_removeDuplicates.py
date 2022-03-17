bff2eae341c755dd0fd608f4645fce7310a9fa76
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-17 10:47:55 -0500

    Update 16_removeDuplicates.py

--- 16_removeDuplicates.py
+++ 16_removeDuplicates.py
@@ -1,9 +1,12 @@
+from os import removedirs
+
+
 def remove_duplicates(list1):
     # edgecase: if list is empty
     if not list1:
         return 'please add elements to list'
 
-    # remove the duplicates with a set
+    # return a set of the list - sets remove duplicates
     return list(set(list1))
 
-print(remove_duplicates([1,2,3,4,4,4,4]))
+print(remove_duplicates([1,2,3,3,3]))
