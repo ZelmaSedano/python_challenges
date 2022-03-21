ba5f5e4d45873b538e0a148e8c91de33c5ea1d11
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-21 11:23:54 -0500

    Update 49_squareDigits.py

--- 49_squareDigits.py
+++ 49_squareDigits.py
@@ -1,14 +1,26 @@
 def square_digits(num):
+    # edgecase: if num is negative
+    if num < 0:
+        return 'please only use positive numbers'
+
+    # create a result variable to return
     result = []
 
-    words = list(str(num))
+    # cast the num into a string, and split it
+    arr = list(str(num))
 
-    for i in words:
+    # loop through the list
+    for i in arr:
+        # create a variable to hold the number version of i 
         y = int(i)
+
+        # square the number and add it to result list as string
         result.append(str(y*y))
 
+    # create a new string with the joined result
     newStr = ''.join(result)
-    return int(newStr)
 
+    # return the string converted to an integer
+    return int(newStr)
 
 print(square_digits(13))
