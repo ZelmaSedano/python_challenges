2a66350758d3f37e5155c61205c2ceb9590abeaf
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-21 10:45:26 -0500

    Update 58_isUnique.py

--- 58_isUnique.py
+++ 58_isUnique.py
@@ -1,10 +1,10 @@
-def is_string_unique(str):
+def is_str_unique(str):
     # edgecase: if string is empty
-    if not str:
+    if not str or str.isspace():
         return 'please add letters to string'
 
-    # when you create a set in python, it doesn't keep the original order of the elements when it's a string
-    # so you have to sort them
+    # remove all the duplicates from the string using set and compare it to str
+    # when you use set in python, it doesn't keep the original order of the letters, so order them
     return sorted(set(str)) == sorted(str)
 
-print(is_string_unique('hi'))
+print(is_str_unique('helo'))
