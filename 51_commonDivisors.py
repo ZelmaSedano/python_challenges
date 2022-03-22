c6b38593df7b8dd54ddb65274c81fa4ead76695a
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-22 18:17:49 -0500

    Update 51_commonDivisors.py

--- 51_commonDivisors.py
+++ 51_commonDivisors.py
@@ -1,18 +1,14 @@
-def common_divisors(num1, num2):
-    # edgecase: if one number is positive and one is negative, they aren't divisors
-    if num1 < 0 and num2 > 0:
-        return 'please only use positive numbers'
-    elif num1 > 0 and num2 < 0:
-        return 'please only use positive numbers'
+def leetcode_minimum_rotated(list):
+    # define what index we're going to rotate by based off first element
+    index = list[0]
 
-    # create a result variable to return
-    result = []
+    # rotate the list by index #
+    # slice off index to end, then add the elements from 0 to index
+    # 3,4,5,6,7 --> 6,7,3,4,5
+    # 6 = element at index 3 (3=0, 4=1, 5=2, 6=3)
+    # 6,7 + 3,4,5 
+    result = list[index:len(list)] + list[0:index]
 
-    # loop from 2 to num2, including num2
-    for i in range(1, num2+1):
-        if num1 % i == 0 and num2 % i == 0:
-            result.append(i)
+    return min(result)
 
-    return result
-
-print(common_divisors(6, 18))
+print(leetcode_minimum_rotated([3,4,5,6,7]))
