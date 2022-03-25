a7ad6b67da3b6d1f84481d5aee577a9d59155c0c
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-25 09:34:01 -0500

    Update 72_leetcode_min_rotated.py

--- 72_leetcode_min_rotated.py
+++ 72_leetcode_min_rotated.py
@@ -1,12 +1,16 @@
-https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+def leetcode_min_rotated(list):
+    # edgecase: if list is empty
+    if not list:
+        return 'please add elements to list'
 
-def truncate_str(str, num):
-    # edgecase: if string is empty or num is negative
-    if not str or str.isspace():
-        return 'please add letters to string'
-    if num < 0:
-        return 'please only use positive numbers'
+    # you want to rotate the list by whatever is the first number in the list
+    # create a variable to hold that number
+    index = list[0]
 
-    return str[0:num] + '...'
+    # create a new list that is rotated by index
+    # you want the last part of the original list + first part up to index
+    arr = list[index: len(list)] + list[0:index]
 
-print(truncate_str('hello', 3))
+    return arr
+
+print(leetcode_min_rotated([3,4,5,6,7]))
