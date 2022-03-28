b4b522c369bebd5c0cd848ecd9bbd3b6039277f7
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-03-28 13:29:21 -0500

    Update 76_rotate_list.py

--- 76_rotate_list.py
+++ 76_rotate_list.py
@@ -1,13 +1,13 @@
-def rotate_list(list, num):
-    # edgecases: if list is empty or num is negative
-    if not list:
+def rotate_list_left(list, num):
+    # edgecase: if list is empty or num is negative
+    if not list and num < 0:
+        return 'please add elements to list and make sure to use postiive numbers for 2nd arg'
+    elif not list:
         return 'please add elements to list'
-    if num < 0:
-        return 'please only use positive numbers'
+    elif num < 0:
+        return 'please only use positive numbers for 2nd arg'
 
-    # using the number, rotate the list
-    result = list[num:len(list)] + list[0:num]
-
+    result = list[len(list)-num:len(list)] + list[0:len(list)-num]
     return result
 
-print(rotate_list([0,1,2,3,4,5], 3))
+print(rotate_list_left([0,1,2,3,4], 1))
