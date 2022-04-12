45ee2aec84b4357c7c3bdd4b9b9c32e34194f5ab
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-12 13:27:44 -0500

    Update 12_factorial.py

--- 12_factorial.py
+++ 12_factorial.py
@@ -1,13 +1,12 @@
 def factorial(num):
-    # edgecases: negative numbers don't have a factorial
+    # edgecase: if num is negative b/c negative factorials are considered complex numbers
     if num < 0:
-        return 'negative numbers do not have a factorial'
-
-    # if num is 0, factorial is 1
+        return 'please only use positive numbers b/c negative factorials are complex numbers'
+    # if num is 0, return 1 b/c the factorial of 0 is 1 
     if num == 0:
         return 1
 
-    # create a result variable to hold result & set it to 1 b/c we're multiplying by it
+    # create a result variable & set it to 1 b/c we're multiplying by it
     result = 1
 
     # loop from 1 to num, including num
@@ -15,5 +14,5 @@ def factorial(num):
         result *= i
 
     return result
-
+    
 print(factorial(5))
