e463cd24010adff6386e3af8bf88afa1115d1b1f
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-12 13:32:00 -0500

    Update 9_isInteger.py

--- 9_isInteger.py
+++ 9_isInteger.py
@@ -1,6 +1,8 @@
 def is_integer(num):
-    # integers can be negative, so no edgecase
+    # edgecase: don't enter decimals
+    if isinstance(num, float):
+        return 'please only use whole numbers'
 
     return type(num) == int
 
-print(is_integer('hi'))
+print(is_integer(-17))
