77fa2f3ae8e9cb40e4b85a479b552b846dca55aa
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-13 17:14:35 -0500

    Update 13_consecutiveInts.py

--- 13_consecutiveInts.py
+++ 13_consecutiveInts.py
@@ -3,8 +3,10 @@ def consecutive_ints(list):
         return 'please add elements to list'
     # return any(list[i] == list[i+1] for i in range(len(list)-1))
     # do not build an iterate over an elements
-    for i in list:
-      # is the current value the same as the next value
-      return i == i
+    for i in range(len(list)-1):
+      if list[i] == list[i+1]:
+        return True
+
+    return False
 
 print(consecutive_ints([22,23,8,8,8,1]))
