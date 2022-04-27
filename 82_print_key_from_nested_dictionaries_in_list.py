eaae9c87ba7c7849383463b99aca582e8f86006f
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-04-27 10:07:39 -0500

    Update 82_print_key_from_nested_dictionaries_in_list.py

--- 82_print_key_from_nested_dictionaries_in_list.py
+++ 82_print_key_from_nested_dictionaries_in_list.py
@@ -2,7 +2,11 @@ def get_value_key_in_list_nested_dictionary(list):
     # edgecase: if list is empty
     if not list:
         return 'please add multiple nested dictionaries to list'
+    # you can either create an empty list and add each value to it, or print each value
+    result = []
     for key in list:
-        print(key['color'])
+        result.append(key['color'])
+
+    return result
 
 print(get_value_key_in_list_nested_dictionary([{'color': 'blue', 'fruit': 'apple', 'pet': 'dog'}, {'color': 'purple', 'fruit': 'apple', 'pet': 'dog'}, {'color': 'red', 'fruit': 'apple', 'pet': 'dog'}, {'color': 'violet', 'fruit': 'apple', 'pet': 'dog'}]))
