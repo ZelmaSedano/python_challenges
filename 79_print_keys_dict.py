047a058719f4c9bcfcc51d3d5e35e77b988abd1e
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-05-03 10:59:23 -0500

    Update 79_print_keys_dict.py

--- 79_print_keys_dict.py
+++ 79_print_keys_dict.py
@@ -1,4 +1,11 @@
 def print_keys_dict(dict):
+    # edgecase: if dictionary is empty
+    if not dict:
+        # you have to use print, otherwise this won't show up
+        # you can return, but you'd have to call print on line 12 
+        print('please add elements to dictionary')
+    
+    # loop through dictionary using for
     for key in dict:
         print(key)
 
