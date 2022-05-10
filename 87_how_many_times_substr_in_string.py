d3dfda1322175c90696c6765b3d626caac6f11eb
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2022-05-10 14:01:32 -0500

    Create 87_how_many_times_substr_in_string.py

---
+++ 87_how_many_times_substr_in_string.py
@@ -0,0 +1,5 @@
+# check to see how many times a string appears inside a string
+def how_many_times_substring(string, substring):
+    return string.count(substring)
+
+print(how_many_times_substring('hey hey hey', 'hey'))
