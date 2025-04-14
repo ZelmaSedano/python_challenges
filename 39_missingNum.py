32bc93ac1fad1f0f99a6b81240fd7feda3e8c347
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-04-14 08:44:37 -0500

    Update 39_missingNum.py

--- 39_missingNum.py
+++ 39_missingNum.py
@@ -5,11 +5,8 @@ def missing_num(list):
     # loop through list using index
     # the reason you don't have to use len(list)-1 is because you're not comparing the current element w/ the next element
     for i in list:
-        # list[i+1] = next element in list
-        # list[i]+1 = value + 1
-        # if the next element in list is NOT current element's value + 1
+        # if the next element ISN'T the same as the current element's value +1
         if list[i+1] != list[i]+1:
-            # return current element's value + 1
             return list[i]+1
 
 print(missing_num([1,2,3,4,6]))
