f6aa1b489ca42b01f588f480e19a49c1057a0a24
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-04-22 08:09:53 -0500

    Update 39_missingNum.py

--- 39_missingNum.py
+++ 39_missingNum.py
@@ -1,12 +1,6 @@
 def missing_num(list):
-    # edgecase: if list is empty
-    if not list:
-        return 'please add elements to list'
-    # loop through list using index
-    # the reason you don't have to use len(list)-1 is because you're not comparing the current element w/ the next element
-    for i in list:
-        # if the next element ISN'T the same as the current element's value +1
+    for i in range(len(list)):
         if list[i+1] != list[i]+1:
             return list[i]+1
-
-print(missing_num([1,2,3,4,6]))
+        
+print(missing_num([1,2,5]))
