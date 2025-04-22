1d2aeca18f35affeeb82d547555f3d31aeb717d6
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-04-22 08:17:51 -0500

    Update 39_missingNum.py

--- 39_missingNum.py
+++ 39_missingNum.py
@@ -1,5 +1,6 @@
 def missing_num(list):
-    # I originally used the 'for i in list' loop, BUT that will result in an error when it reaches the end of the list
+    # I originally used the 'for i in list' loop
+    # BUT that will result in an error when it reaches the end of the list
     for i in range(len(list)):
         if list[i+1] != list[i]+1:
             return list[i]+1
