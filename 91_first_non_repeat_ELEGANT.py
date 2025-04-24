6993b3d960ea74e3a7a3ee3599083144f9dacd4b
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-04-24 14:30:09 -0500

    Update 91_first_non_repeat_ELEGANT.py

--- 91_first_non_repeat_ELEGANT.py
+++ 91_first_non_repeat_ELEGANT.py
@@ -1,11 +1,11 @@
 def first_non_repeat(string):
     # create an empty list
-    arr = {}
+    obj = {}
 
-    # loop through the string, if the current element is in the list/arr, then add to its value
+    # loop through the string, if the current element is in the list/obj, then add to its value
     for char in string:
         # check to see if char is in list
-        if char in arr:
+        if char in obj:
             arr[char] += 1
         else:
             arr[char] = 1
@@ -13,7 +13,7 @@ def first_non_repeat(string):
     # loop through the string
     for char in string:
         # if the current element is ALSO in array, and it's value is 1, return it
-        if arr[char] == 1:
+        if obj[char] == 1:
             return char
         
     # if there aren't any non-repeating chars
