2289462aa44d36cbbfb9831f2d8fc31e73a5a4db
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-04-29 13:25:25 -0500

    Update 5_wordCount.py

--- 5_wordCount.py
+++ 5_wordCount.py
@@ -7,14 +7,17 @@ def word_count(string):
 
     # elegant version
     # words = [word for word in string.split(' ') if word != '']
-    
+
+    # create an empty array/list, which we'll push letters/words into
     words = []
+
+    # loop through the split string
     for word in string.split(' '):
         if word != '':
             words.append(word)
     # if word isn't a space, add it to a new array called 'words'
     
-    # Return the count of remaining valid words
+    # return the length of words array/list
     return len(words)
 
 print(word_count('hi there    ')) 
