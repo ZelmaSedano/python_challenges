91fff39e2b56cd2300b97f989653bc9bed9b3709
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-05-05 10:58:03 -0500

    Update 35_firstNonRepeat.py

--- 35_firstNonRepeat.py
+++ 35_firstNonRepeat.py
@@ -1,21 +1,16 @@
-# brute force - not as effcient
-def first_non_repeat(string):
-    # I like the brute-force approach
-    # if there's only one of the letters in the entire string, then it is the first-non-repeating since it'll be the first to return
+def first_non_repeat_brute(string):
+    # remove spaces? - remove all spaces
+    # split? naw
 
-    # loop through the string
-    for i in string:
-        # if the current element's value count is 1, return it
-        # it'll be the first non-repeat since we're looping through
-        if char != ' ' and string.count(i) == 1:
+    string1 = string.replace(' ', '')
+
+    # loop through new string 
+    # do we need to access indices? - NO
+    for i in string1:
+        if string1.count(i) == 1:
             return i
-            
-    # EVEN SIMPLER, no math logic
-    for num in list:
-        if num+1 not in list:
-            return num+1
         
-    # if there are no non-repeating chars
+    # if there are no non-repeating characters
     return 'no non-repeating characters'
 
-print(first_non_repeat('nono'))
+print(first_non_repeat_brute('hi therei'))
