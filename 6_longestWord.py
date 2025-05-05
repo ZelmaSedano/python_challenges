c5c50f286afc44816458e63511b90a6a1515d051
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-05-05 10:47:41 -0500

    Update 6_longestWord.py

--- 6_longestWord.py
+++ 6_longestWord.py
@@ -1,17 +1,15 @@
 def longest_word(string):
-    # do you need to remove all spaces, between spaces, or trailing spaces?
-    # - you can't remove all spaces, b/c you're counting longest word
-    # test to see if between spaces show up
-    # removing trailing spaces would be a good idea
-    string1 = string.strip()
+    # remove all trailing and between spaces
+    words = string.split()
 
+    # create an empty variable to hold longest word
     longest = ''
 
-    # .split(' ') takes care of the BETWEEN spaces
-    for word in string1.split(' '):
+    # loop through the list of words
+    for word in words:
         if len(word) > len(longest):
             longest = word
+    
+    return longest
 
-    return len(longest)
-
-print(longest_word('hi             there'))
+print(longest_word('hi there'))
