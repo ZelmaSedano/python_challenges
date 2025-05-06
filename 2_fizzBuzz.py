80e26ff5b7318137eaa291390878df4dcb5203a8
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-05-06 15:50:16 -0500

    Update 2_fizzBuzz.py

--- 2_fizzBuzz.py
+++ 2_fizzBuzz.py
@@ -1,13 +1,12 @@
-def fizzBuzz(n):
-  for i in range(1, n):
-    if i % 15 == 0:
-      print('fizzBuzz')
-    elif i % 3 == 0:
-      print('fizz')
-    elif i % 5 == 0:
-      print('buzz')
-    else:
-      print(i)
+def fizzbuzz(n):
+    for i in range(1,n+1):
+        if i % 15 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
+            print('fizz')
+        elif i % 5 == 0:
+            print('buzz')
+        else:
+            print(i)
 
-
-fizzBuzz(15)
+fizzbuzz(20)
