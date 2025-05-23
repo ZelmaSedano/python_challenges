913e3e7349af94e7e89faf7bcb2b2c60c1dc2b55
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-05-23 09:13:16 -0500

    Update 95_remove_item_from_list.py

--- 95_remove_item_from_list.py
+++ 95_remove_item_from_list.py
@@ -8,3 +8,11 @@ def remove_element_from_list(lst, target):
     return result
 
 print(remove_element_from_list([1,2,3,4], 3))
+
+# more elegent way
+def remove_element_list(lst, element):
+    lst.remove(element)
+
+    return lst
+
+print(remove_element_list([1,2,3,4], 3))
