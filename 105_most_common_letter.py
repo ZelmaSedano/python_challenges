88f95826f2c73f2b05cf4a7f53e445935bd31363
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-11-18 09:07:12 -0600

    Refactor most_common_letter function logic
    Refactor most_common_letter function to use a single dictionary for letter counts and handle cases with no most common letter.

--- 105_most_common_letter.py
+++ 105_most_common_letter.py
@@ -1,16 +1,23 @@
 def most_common_letter(string):
-    # create a dictionary to push the string & value into
-    most_common = {}
 
-    # loop through the string:
+    # create an empty object/dictionary
+    letter={}
+
+    # loop through the string
     for i in string:
-        # if the character already exists in the dictionary, add to its value
-        if i in most_common:
-            most_common[i] += 1
+        # if the letter is in the dictionary, add to its value
+        if i in letter:
+            letter[i] += 1
+        # if it's not in the dictionary yet, add it and make its value 1
         else:
-            # adds the i element with a value of 1
-            most_common[i] = 1
+            letter[i] = 1
 
-    return max(most_common, key=most_common.get)
+    # there might not be a most_common_letter, they may all be equally present
+    # use a set, a set doesn't allow duplicates
+    if(len(set(letter.values()))) == 1:
+        return 'no most-common-letter'
 
-print(most_common_letter('hiii'))
+    #max(key=letter.get) === find the key in the dictionary with the highest value
+    return max(letter, key=letter.get)
+
+print(most_common_letter('hhii'))
