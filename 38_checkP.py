d5cb983233494e8b47645ccb7847e4c4b525b1e4
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-12-05 08:15:43 -0600

    Refactor checkP function to check_p with improvements

--- 38_checkP.py
+++ 38_checkP.py
@@ -1,16 +1,19 @@
-def checkP(string):
-    if len(string) == 0 or 
+def check_p(string):
+    # if there aren't any parentheses
+    if '(' not in string and ')' not in string:
+        return 'no parentheses'
     
-    # create a counter variable
+    # create a count variable
     count = 0
 
-    # loop through string, looking for ( & )
+    # loop through the string
     for i in string:
         if i == '(':
-            count += 1
+            count+=1 
         elif i == ')':
-            count -= 1
+            count-=1
 
+    # if there are equal parentheses,return x
     return count == 0
 
-print(checkP('hi'))
+print(check_p(''))
